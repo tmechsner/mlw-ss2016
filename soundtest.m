f8d91af9e7634fc0bf42248c3fdb3b54bd679146
David Leins <dleins@techfak.uni-bielefeld.de>
2016-06-24 16:56:58 +0200

    Added comparison with error evaluation.

--- soundtest.m
+++ soundtest.m
@@ -26,40 +26,31 @@ if(ismember(1,execExperiments))
 
     % Time scale (x axis)
     t = [1/fs:1/fs:length(norm1)/fs];
-
-    figure
-    subplot(3,2,1)
-     plot(t, mixedsig(1, :))
-     ylim([-1 1])
-     xlim([0 6])
-     title('Mixed sources 1')
-    subplot(3,2,2)
-     plot(t, mixedsig(2, :))
-     ylim([-1 1])
-     xlim([0 6])
-     title('Mixed sources 2')
-    subplot(3,2,3)
-     plot(t, norm1)
-     ylim([-1 1])
-     xlim([0 6])
-     title('Computed source 1')
-    subplot(3,2,4)
-     plot(t, norm2)
-     ylim([-1 1])
-     xlim([0 6])
-     title('Computed source 2')
-    subplot(3,2,5)
-     plot(t, signal(1,:))
-     ylim([-1 1])
-     xlim([0 6])
-     title('Original source 1')
-    subplot(3,2,6)
-     plot(t, signal(2,:))
-     ylim([-1 1])
-     xlim([0 6])
-     title('Original source 2')
-
+    
+    icaplot('complot', signal, 0, 0, 0, 'Original Signals')
     pause;
+    icaplot('complot', mixedsig, 0, 0, 0, 'Noisy mixed Signals')
+    pause;
+    icaplot('complot', decompose, 0, 0, 0, 'Decomposed Signals')
+    
+    prompt = {'Enter signal number:','Enter source number:'};
+    dlg_title = 'Signal / Input comparison';
+    num_lines = 1;
+    
+    pause;
+    answer = inputdlg(prompt,dlg_title,num_lines);
+        n1 = str2num(answer{1});
+        n2 = str2num(answer{2});
+    
+    while n1 > 0
+        icaplot('sumerror', signal, n1, decompose, n2, 0, 0, 'Error comparison')
+        pause;
+
+        answer = inputdlg(prompt,dlg_title,num_lines);
+        n1 = str2num(answer{1});
+        n2 = str2num(answer{2});
+    end
+    
     close all;
 end
 
@@ -145,13 +136,30 @@ if(ismember(3,execExperiments))
     norm1 = source1/10;
     norm2 = source2/10;
 
-    icaplot('complot', signal, 0, 0, 0, 'Original Signals')
+     icaplot('complot', signal, 0, 0, 0, 'Original Signals')
+     pause;
+     icaplot('complot', mixedsig, 0, 0, 0, 'Mixed Signals')
+     pause;
+     icaplot('complot', decompose, 0, 0, 0, 'Decomposed Signals')
+    
+     prompt = {'Enter signal number:','Enter source number:'};
+    dlg_title = 'Signal / Input comparison';
+    num_lines = 1;
+    
     pause;
-    icaplot('complot', mixedsig, 0, 0, 0, 'Mixed Signals')
-    pause;
-    icaplot('complot', decompose, 0, 0, 0, 'Decomposed Signals')
+    answer = inputdlg(prompt,dlg_title,num_lines);
+        n1 = str2num(answer{1});
+        n2 = str2num(answer{2});
+    
+    while n1 > 0
+        icaplot('sumerror', signal, n1, decompose, n2, 0, 0, 'Test')
+        pause;
 
-    pause;
+        answer = inputdlg(prompt,dlg_title,num_lines);
+        n1 = str2num(answer{1});
+        n2 = str2num(answer{2});
+    end
+    
     close all;
 end
 
@@ -166,7 +174,7 @@ if(ismember(4,execExperiments))
     noisySignal = addNoise(signal, 1.0);
     mixmat = rand(size(signal,1));
     mixedNoisySignals = mixmat * signal;
-
+    
     decompose = fastica(mixedNoisySignals);
 
     source1 = decompose(1,:);
@@ -181,7 +189,24 @@ if(ismember(4,execExperiments))
     pause;
     icaplot('complot', decompose, 0, 0, 0, 'Decomposed Signals')
 
+    prompt = {'Enter signal number:','Enter source number:'};
+    dlg_title = 'Signal / Input comparison';
+    num_lines = 1;
+    
     pause;
+    answer = inputdlg(prompt,dlg_title,num_lines);
+        n1 = str2num(answer{1});
+        n2 = str2num(answer{2});
+    
+    while n1 > 0
+        icaplot('sumerror', signal, n1, decompose, n2, 0, 0, 'Test')
+        pause;
+
+        answer = inputdlg(prompt,dlg_title,num_lines);
+        n1 = str2num(answer{1});
+        n2 = str2num(answer{2});
+    end
+
     close all;
 end
 
@@ -210,6 +235,23 @@ if(ismember(5,execExperiments))
     pause;
     icaplot('complot', decompose, 0, 0, 0, 'Decomposed Signals')
 
+    prompt = {'Enter signal number:','Enter source number:'};
+    dlg_title = 'Signal / Input comparison';
+    num_lines = 1;
+    
     pause;
+    answer = inputdlg(prompt,dlg_title,num_lines);
+        n1 = str2num(answer{1});
+        n2 = str2num(answer{2});
+    
+    while n1 > 0
+        icaplot('sumerror', signal, n1, decompose, n2, 0, 0, 'Test')
+        pause;
+
+        answer = inputdlg(prompt,dlg_title,num_lines);
+        n1 = str2num(answer{1});
+        n2 = str2num(answer{2});
+    end
+    
     close all;
 end
