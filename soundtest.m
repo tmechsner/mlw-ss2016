aa7e0b2fdfe651bbf1f3a80dcaed2f12129f7afd
David Leins <dleins@techfak.uni-bielefeld.de>
2016-06-29 21:33:54 +0200

    Adds pca. ica comparison as experiment nr. 7

--- soundtest.m
+++ soundtest.m
@@ -172,7 +172,7 @@ if(ismember(3,execExperiments))
         n2 = str2num(answer{2});
     
     while n1 > 0
-        icaplot('sumerror', signal, n1, decompose, n2, 0, 0, 'Test')
+        icaplot('sumerror', signal, n1, decompose, n2, 0, 0, 'Comparison')
         pause;
 
         answer = inputdlg(prompt,dlg_title,num_lines);
@@ -211,7 +211,7 @@ if(ismember(4,execExperiments))
         n2 = str2num(answer{2});
     
     while n1 > 0
-        icaplot('sumerror', signal, n1, decompose, n2, 0, 0, 'Test')
+        icaplot('sumerror', signal, n1, decompose, n2, 0, 0, 'Comparison')
         pause;
 
         answer = inputdlg(prompt,dlg_title,num_lines);
@@ -249,7 +249,7 @@ if(ismember(5,execExperiments))
         n2 = str2num(answer{2});
     
     while n1 > 0
-        icaplot('sumerror', signal, n1, decompose, n2, 0, 0, 'Test')
+        icaplot('sumerror', signal, n1, decompose, n2, 0, 0, 'Comparison')
         pause;
 
         answer = inputdlg(prompt,dlg_title,num_lines);
@@ -288,7 +288,7 @@ if(ismember(6,execExperiments))
     %    n2 = str2num(answer{2});
     
     %while n1 > 0
-    %    icaplot('sumerror', signal, n1, decompose, n2, 0, 0, 'Test')
+    %    icaplot('sumerror', signal, n1, decompose, n2, 0, 0, 'Comparison')
     %    pause;
 %
  %       answer = inputdlg(prompt,dlg_title,num_lines);
@@ -297,4 +297,51 @@ if(ismember(6,execExperiments))
     %end
     
     close all;
+    
+end
+    
+if(ismember(7,execExperiments))
+    
+    [signal(1,:), fs] = audioread('source3.wav')
+    [signal(2,:), fs] = audioread('source7.wav');
+
+    % Mix signals
+    mixmat = rand(size(signal,1));
+    mixedsig = mixmat * signal;
+
+    [decompose,mapping] = compute_mapping(mixedsig', 'PCA', 2);
+
+    source1 = decompose(1,:);
+    source2 = decompose(2,:);
+
+    norm1 = source1/10;
+    norm2 = source2/10;
+
+    % Time scale (x axis)
+    t = [1/fs:1/fs:length(norm1)/fs];
+    
+    icaplot('complot', signal, 0, 0, 0, 'Original Signals')
+    icaplot('complot', mixedsig, 0, 0, 0, 'Mixed Signals')
+    icaplot('complot', decompose', 0, 0, 0, 'Decomposed Signals')
+
+    prompt = {'Enter signal number:','Enter source number:'};
+    dlg_title = 'Signal / Input comparison';
+    num_lines = 1;
+    
+    pause;
+    answer = inputdlg(prompt,dlg_title,num_lines);
+        n1 = str2num(answer{1});
+        n2 = str2num(answer{2});
+    
+    while n1 > 0
+        icaplot('sumerror', signal, n1, decompose', n2, 0, 0, 'Comparison')
+        pause;
+
+        answer = inputdlg(prompt,dlg_title,num_lines);
+        n1 = str2num(answer{1});
+        n2 = str2num(answer{2});
+    end
+    
+    close all;
+    
 end
